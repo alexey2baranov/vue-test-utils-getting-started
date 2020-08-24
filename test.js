7590d9296a7edf566bc68548ca3425d320c0b6e1
alexey2baranov <Ir1n@gh>
2020-08-25 02:06:08 +0300

    v3

--- test.js
+++ test.js
@@ -1,34 +1,28 @@
 import {mount} from '@vue/test-utils'
 import User from './user'
-import User2 from './user2'
-import waitForExpect from "wait-for-expect";
 import flushPromises from "flush-promises/index";
+import waitForExpect from "wait-for-expect";
 
 describe('user', () => {
     it('should update rendered html after user.name fetched', async () => {
+        const user = {
+            name: 'undefined_name'
+        }
+
         // mount component that includes user fetching
         const wrapper = mount(User, {
             propsData: {
-                value: {
-                    name: 'undefined_name',
-                },
+                value: user,
             }
         })
         await flushPromises()
-        expect(wrapper.text()).toContain('fetched_name')
-        // test component's html
-        // await waitForExpect(() => {
-        //     expect(wrapper.text()).toContain('fetched_name')
-        // })
-    })
+        expect(wrapper.text()).toContain('undefined_name')
 
-    it('should update rendered html after plain user fetched', async () => {
-        // mount component that includes user fetching
-        const wrapper = mount(User2, {
-            propsData: {
-                value: 'undefined_name',
-            }
-        })
+        console.log('emulating fetching user data ...')
+        await new Promise(resolve => setTimeout(resolve, 0))
+        user.name = 'fetched_name'
+        console.log('user updated')
+
         await flushPromises()
         expect(wrapper.text()).toContain('fetched_name')
     })
--- user.js
+++ user.js
@@ -10,10 +10,5 @@ export default {
         }
     },
     async mounted() {
-        // fetch user emulation
-        console.log('user fetching...')
-        // await new Promise(resolve => setTimeout(resolve, 1000))
-        this.value.name = 'fetched_name'
-        console.log('user fetched')
     }
 }
